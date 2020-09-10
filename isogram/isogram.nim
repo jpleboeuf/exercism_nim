31d80c6211459f291fca597c812f56980cb6c11a
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-09-10 21:50:35 +0200

    Solve exercise "Isogram" (cont.)
    Made the code faster
     by converting to lowercase
     only when encountering an alphabetical character
     (therefore not converting the full word upfront to lowercase).

--- isogram/isogram.nim
+++ isogram/isogram.nim
@@ -5,8 +5,9 @@ proc alphaPos(c: char): int {.inline.} =
 
 proc isIsogram*(s: string): bool {.inline.} =
   var alphagram: array[1..26, int]
-  for l in toLowerAscii(s):
-    if isAlphaAscii(l):
+  for c in s:
+    if isAlphaAscii(c):
+      var l = toLowerAscii(c)
       inc(alphagram[alphaPos(l)])
       if alphagram[alphaPos(l)] > 1:
         return false
