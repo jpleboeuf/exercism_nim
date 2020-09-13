543600abfefc40e0f3490d84472194fe25c57d2f
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-09-13 13:57:57 +0200

    Solve exercise "Hamming" (cont.)
    Made the code shorter by replacing the loop by a filter-ing.

--- hamming/hamming.nim
+++ hamming/hamming.nim
@@ -1,10 +1,7 @@
-from sequtils import zip
+import sugar
+from sequtils import zip, filter
 
 proc distance*(dna1: string, dna2: string): int =
-  var d = 0
   if dna1.len != dna2.len:
     raise newException(ValueError, "sequences not of equal length")
-  else:
-    for (n1, n2) in zip(@dna1, @dna2).items:
-      if n1 != n2: inc(d)
-  d
+  zip(@dna1, @dna2).filter(n => n[0] != n[1]).len
