caa78b4aef30293806f6477cf82adc927e944c0c
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-09-14 01:07:42 +0200

    Solve exercise "Word Count" (cont.)
    Made the code a lot shorter
     by replacing all the if-ing and looping by filter-ing and map-ing.

--- word-count/word_count.nim
+++ word-count/word_count.nim
@@ -1,32 +1,10 @@
-from strutils import toLowerAscii, isAlphaNumeric, splitWhitespace
+from sequtils import filter, map
+import sugar
+from strutils import AllChars, Letters, Digits
+from strutils import toLowerAscii, split, strip
 from tables import CountTable, toCountTable
 
 proc countWords*(str: string): CountTable[string] =
-  var str_clean = ""
-  for i, c in str:
-    # The count is case insensitive:
-    var c = toLowerAscii(c)
-    # Let's handle the apostrophe case:
-    if c == '\'':
-      # An apostrophe at one of the bounds of the string
-      #  is not in a contraction;
-      #  therefore, treat it as a space:
-      if i == 0 or i == str.len-1:
-        c = ' '
-      # An apostrophe in a contraction is okay;
-      #  therefore, keep it:
-      elif isAlphaNumeric(str[i-1]) and isAlphaNumeric(str[i+1]):
-        c = '\''
-      # In all other cases, treat the apostrophe as a space:
-      else:
-        c = ' '
-    # Let's handle other non alphanumeric characters;
-    #  treat them as space:
-    elif not isAlphaNumeric(c):
-      c = ' '
-    # Character cleaning done,
-    #  let's go on building the cleaned string:
-    str_clean &= c
-  # Create the hash table storing the word count:
-  var words = str_clean.splitWhitespace()
+  const nonWordChars = AllChars - Letters - Digits - {'\''}
+  var words = toLowerAscii(str).split(nonWordChars).filter(s => s.len > 0).map(w => w.strip(chars = {'\''}))
   words.toCountTable()
