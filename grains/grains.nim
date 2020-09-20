d246db87595fa89b233f3ab8148cfb472f091c57
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-09-20 22:57:03 +0200

    Solve exercise "Grains" (cont.)
    Used some math and mapIt-ing.

--- grains/grains.nim
+++ grains/grains.nim
@@ -1,16 +1,10 @@
-proc onSquare*(sqIdx: int): uint =
+from math import `^`, sum
+from sequtils import toSeq, mapIt
+
+func onSquare*(sqIdx: int): uint =
   if sqIdx <= 0 or sqIdx > 64:
     raise newException(ValueError, "square index has to be between 1 and 64")
-  const initAmount: uint = 1
-  var amount: uint = initAmount
-  var prevAmount: uint = amount
-  for _ in countup(2, sqIdx):
-    amount = 2 * prevAmount
-    prevAmount = amount
-  amount
+  result = uint(2) ^ uint(sqIdx - 1)
 
-proc total*(): uint =
-  var tot: uint = 0
-  for sqIdx in countup(1, 64):
-    tot += onSquare(sqIdx)
-  tot
+func total*(): uint =
+  sum toSeq(1..64).mapIt(onSquare(it))
