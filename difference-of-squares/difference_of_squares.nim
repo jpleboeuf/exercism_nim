58069630b134f0f524dc837953629649a30e283e
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-09-22 01:14:29 +0200

    Solve exercise "Difference Of Squares" (cont.)
    Moved the declaration of the exception to a template
     to get ride of the "side effects" error
     in order to later use pure functions.

--- difference-of-squares/difference_of_squares.nim
+++ difference-of-squares/difference_of_squares.nim
@@ -1,22 +1,20 @@
-var ve = newException(ValueError, "n has to be a natural number")
+template raiseValueError(): untyped =
+  raise newException(ValueError, "n has to be a natural number")
 
 proc squareOfSum*(n: uint): uint =
-  if n < 1:
-    raise ve
+  if n < 1: raiseValueError
   var sum: uint = 0
   for i in countup(uint(1), n):
       sum += i
   sum * sum
 
 proc sumOfSquares*(n: uint): uint =
-  if n < 1:
-    raise ve
+  if n < 1: raiseValueError
   var sum: uint = 0
   for i in countup(uint(1), n):
     sum += i * i
   sum
 
 proc difference*(n: uint): uint =
-  if n < 1:
-    raise ve
+  if n < 1: raiseValueError
   squareOfSum(n) - sumOfSquares(n)
