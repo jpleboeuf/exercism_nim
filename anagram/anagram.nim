f1f211effd82f3d6f3e21742e022b97198a4b017
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-09-24 00:17:05 +0200

    Solve exercise "Anagram" (cont.)
    Did a small refactoring.

--- anagram/anagram.nim
+++ anagram/anagram.nim
@@ -4,8 +4,8 @@ from algorithm import sorted
 proc detectAnagrams*(word: string, candidates: seq[string]): seq[string] =
   result = @[]
   let wl = toLowerAscii(word)
-  let wl_srtd = sorted(wl, system.cmp[char])
+  let wls = sorted(wl, system.cmp[char])
   for c in candidates:
-    var cl = toLowerAscii(c)
-    if sorted(cl, system.cmp[char]) == wl_srtd and cl != wl:
+    let cl = toLowerAscii(c)
+    if sorted(cl, system.cmp[char]) == wls and cl != wl:
       result.add(c)
