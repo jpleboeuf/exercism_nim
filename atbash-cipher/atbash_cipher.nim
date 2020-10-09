4903b4e24bd737d1914850c0a75c079b4886d4d0
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-10-09 21:39:09 +0200

    Solve exercise "Atbash Cipher" (cont.)
    Switched from doing an on-the-fly character transformation
     to using a pre-generated transformation map
     - should be faster.

--- atbash-cipher/atbash_cipher.nim
+++ atbash-cipher/atbash_cipher.nim
@@ -1,17 +1,29 @@
 import strutils
+import sequtils, algorithm, tables
 from std/wordwrap import wrapWords
 
-const aOrd = ord('a')
-const zOrd = ord('z')
+const transformMap = block:
+  ## Generate the hash table used for encoding/decoding.
+  let
+    upperAlphaChars = toSeq('A'..'Z')
+    lowerAlphaChars = toSeq('a'..'z')
+    alphaChars = upperAlphaChars & lowerAlphaChars
+    digitChars = toSeq('0'..'9')
+    fromChars = alphaChars & digitChars
+    #reverseUpperAlphaChars = reversed(upperAlphaChars)
+    reverseLowerAlphaChars = reversed(lowerAlphaChars)
+    reverseAlphaChars = reverseLowerAlphaChars & reverseLowerAlphaChars
+    toChars = reverseAlphaChars & digitChars
+  var transformMap = initTable[char, char]()
+  for pair in zip(fromChars, toChars):
+    let (fromChar, toChar) = pair
+    transformMap[fromChar] = toChar
+  transformMap
 
 proc transform(str: string): string =
   for c in str:
-    if c.isAlphaAscii:
-      result &= chr(zOrd - (ord(c.toLowerAscii) - aOrd))
-    elif c.isDigit:
-      result &= c
-    else:
-      continue
+    if c.isAlphaNumeric:
+      result &= transformMap[c]
 
 proc encode*(str: string): string =
   wrapWords(transform(str), maxLineWidth = 5, newLine = $' ')
