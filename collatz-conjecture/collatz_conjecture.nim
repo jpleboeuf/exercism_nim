d41ad4d2b98259ad1982e9a0e3cee455536dbaa8
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-10-17 00:25:56 +0200

    Solve exercise "Collatz Conjecture" (cont.)
    Made the code shorter by moving the return condition
     from inside the while loop to the loop condition.

--- collatz-conjecture/collatz_conjecture.nim
+++ collatz-conjecture/collatz_conjecture.nim
@@ -5,9 +5,7 @@ proc steps*(n: int): int =
   if n <= 0: raiseValueError
   var nn: int = n
   var stps: int = 0
-  while true:
-    if nn == 1:
-      return stps
-    else:
-      inc stps
-      if nn mod 2 == 0: nn = nn div 2 else: nn = nn * 3 + 1
+  while nn != 1:
+    inc stps
+    if nn mod 2 == 0: nn = nn div 2 else: nn = nn * 3 + 1
+  stps
