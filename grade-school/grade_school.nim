932198aac2d6fcf367727682322e9e952527db82
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-10-27 22:58:56 +0100

    Solve exercise "Grade School"
    Important note:
     the meaninful solution generate a Nim internal error for now.
    
    See:
     "Why is the chaining order of It templates
     a problem in this example?"
     <https://forum.nim-lang.org/t/6983#43773>,
    and my GitHub comment on the issue linked to this error:
     <https://github.com/nim-lang/Nim/issues/12928#issuecomment-716251308>.

--- grade-school/grade_school.nim
+++ grade-school/grade_school.nim
@@ -1,4 +1,22 @@
+from algorithm import sortedByIt
+from sequtils import unzip, filterIt
+
 type
-  Student* = tuple[name: string, grade: int]
+  Student* = tuple
+    name: string
+    grade: int
   School* = object
     students*: seq[Student]
+
+proc roster*(scool: School): seq[string] =
+  scool.students.sortedByIt((it.grade, it.name)).unzip[0]
+
+proc grade*(scool: School, graad: int): seq[string] =
+  # Does not work
+  #  (see https://github.com/nim-lang/Nim/issues/12928#issuecomment-716251308):
+  #scool.students.filterIt(it.grade == graad).sortedByIt(it.name).unzip[0]
+  # Until a fix, the following works:
+  let stuudents = scool.students.filterIt(it.grade == graad)
+  stuudents.sortedByIt(it.name).unzip[0]
+  # BTW, the following works too, though less meaningful:
+  #scool.students.sortedByIt(it.name).filterIt(it.grade == graad).unzip[0]
