10c7b6561e6acc4fcbea12f31da8245ea8997526
Jean-Philippe Lebœuf <jpl@jean-philippe.leboeuf.name>
2020-11-15 01:11:34 +0100

    Solve exercise "Roman Numerals" (cont.)
    Improved error handling,
     mainly adding support for unknown representations.

--- roman-numerals/roman_numerals.nim
+++ roman-numerals/roman_numerals.nim
@@ -14,23 +14,30 @@ proc roman*(n: int): string =
   for i, nc in $n:
     var nd = parseInt($nc)             # n'digit
     var nd_val = 10 ^ (n_len - (i+1))  # n'digit's value in base 10
-    result &= (
-      case nd:
-        of 0:
-          ""  # skip
-        of 1..3:
-          r[nd_val].repeat(nd)
-        of 4:
-          r[nd_val] & r[nd_val*5]
-        of 5:
-          r[nd_val*5]
-        of 6..8:
-          r[nd_val*5] & r[nd_val].repeat(nd-5)
-        of 9:
-          r[nd_val] & r[nd_val*10]
-        else:
-          raise newException(ValueError, "Not a base 10 digit.")
-      )
+    try:
+      result &= (
+        case nd:
+          of 0:
+            ""  # skip
+          of 1..3:
+            r[nd_val].repeat(nd)
+          of 4:
+            r[nd_val] & r[nd_val*5]
+          of 5:
+            r[nd_val*5]
+          of 6..8:
+            r[nd_val*5] & r[nd_val].repeat(nd-5)
+          of 9:
+            r[nd_val] & r[nd_val*10]
+          else:
+            raise newException(ValueError, $nd & ": not a base 10 digit.")
+        )
+    except KeyError:
+      let e_msg = getCurrentExceptionMsg()
+      let e_msg_prfx = "key not found:"
+      let r_key = e_msg[e_msg.find(e_msg_prfx)+len(e_msg_prfx)+1..^1]
+      echo "Unknown representation for " & r_key & "."
+      result &= "?"
 
 when isMainModule:
   echo roman(2001)
